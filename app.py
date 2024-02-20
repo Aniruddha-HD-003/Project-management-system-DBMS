280435c36279baf55c265898b49092402b981991
Aniruddha-HD-003 <aniruddhahdkedlaya@gmail.com>
2024-02-20 22:58:16 +0530

    added sign.html to app

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return render_template('home.html')
 
+@app.route("/signin")
+def signin():
+    return render_template('sign.html')
+
 if __name__=="__main__":
     app.run(debug=True)
