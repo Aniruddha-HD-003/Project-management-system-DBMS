5e4e72c10ef10022075f57a31412d75bb704159e
Amrutha-surabhi-v <amruthasurabhi22@gmail.com>
2024-03-17 20:17:35 +0530

    connected

--- app.py
+++ app.py
@@ -34,11 +34,56 @@ def inform():
 def project():
     return render_template('projecttable.html')
 
-
+@app.route('/uploadoc')
+def uploadoc():
+    return render_template('uploadoc.html')
 
 @app.route('/signin2')
 def signin2():
     return render_template('signin.html')
+
+@app.route('/task')
+def task():
+    return render_template('task.html')
+@app.route('/taskinsert')
+def taskinsert():
+    return render_template('taskinsert.html')
+@app.route('/taskdel')
+def taskdel():
+    return render_template('taskdel.html')
+@app.route('/teams')
+def teams():
+    return render_template('teams.html')
+
+@app.route('/teaminsert')
+def teaminsrt():
+    return render_template('teaminsert.html')
+
+@app.route('/teamdel')
+def teamdel():
+    return render_template('teamdel.html')
+
+@app.route('/inserttoproj')
+def inserttoproj():
+    return render_template('inserttoproj.html')
+
+@app.route('/deleteproj')
+def deleteproj():
+    return render_template('deleteproj.html')
+
+@app.route('/deletedep')
+def deletedep():
+    return render_template('deletedep.html')
+
+@app.route('/depinsert')
+def depinsert():
+    return render_template('depinsert.html')
+
+@app.route('/departmentdeets')
+def dep():
+    return render_template('departmentdeets.html')
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
